f3c93d5cd44f21df93673aeed95f38b4c8c7a110
Noella Spitz <noella.spitz@gmail.com>
2024-03-15 23:39:01 +0000

    Add tailwind to vite config, create header, remove vite default page

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="fixed bg-white w-full">
+      <div className="flex flex-row justify-around">
+        <img src="/goghlogo.png" width={50}></img>
+      </div>
+    </header>
+  );
+}
