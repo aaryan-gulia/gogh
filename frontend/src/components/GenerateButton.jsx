a53d6b876dfa37d4138ce297a1fbdaaa71762b17
Noella Spitz <noella.spitz@gmail.com>
2024-03-16 15:26:32 +0000

    Increase padding generateButton

--- frontend/src/components/GenerateButton.jsx
+++ frontend/src/components/GenerateButton.jsx
@@ -1,7 +1,7 @@
 export const GenerateButton = () => {
   return (
     <button 
-    className='bg-gradient-to-br from-teal-400 to-emerald-600 hover:from-teal-500 hover:to-emerald-700 rounded-full text-white px-5 h-12 ' 
+    className='bg-gradient-to-br w-min from-teal-400 to-emerald-500 hover:from-teal-400 hover:to-emerald-600 rounded-full text-white px-6 py-2 h-12 ' 
     >
       Generate
     </button>
