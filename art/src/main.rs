df9312bf7cdcf15e226ad7101e596c4dbf81a1c9
0xgleb <gleb.dianov@protonmail.com>
2024-03-16 22:53:18 +0000

    fix export-abi

--- art/src/main.rs
+++ art/src/main.rs
@@ -1,15 +1,22 @@
-use gogh_art::gen::*;
-use stylus_sdk::alloy_primitives::U256;
+#![cfg_attr(not(feature = "export-abi"), no_main)]
 
+#[cfg(feature = "export-abi")]
 fn main() {
-    println!("Generating...");
-
-    let svg = gen(
-        U256::from(RECTANGLES | TRIANGLES | CIRCLES),
-        U256::from(256),
-    );
-
-    std::fs::write("gogh.svg", svg).unwrap();
-
-    println!("Done!");
+    gogh_art::main();
 }
+
+// use gogh_art::gen::*;
+// use stylus_sdk::alloy_primitives::U256;
+
+// fn main() {
+//     println!("Generating...");
+
+//     let svg = gen(
+//         U256::from(RECTANGLES | TRIANGLES | CIRCLES),
+//         U256::from(256),
+//     );
+
+//     std::fs::write("gogh.svg", svg).unwrap();
+
+//     println!("Done!");
+// }
