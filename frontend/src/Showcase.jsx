da7d44b1e54979ebb058e027e52e26d5061048ce
Noella Spitz <noella.spitz@gmail.com>
2024-03-16 23:51:34 +0000

    Implement showcase

--- frontend/src/Showcase.jsx
+++ frontend/src/Showcase.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import searchIcon from "/icons/search.svg"
+
 const nfts = [{
   name: "Nft1",
   tokenId: 1,
@@ -18,7 +21,7 @@ const nftDisplay = (nft) => {
     className="p-8 bg-gray-100 hover:shadow-inner rounded-lg w-fit flex flex-col gap-5"
     >
       <h3 className="font-medium">{nft.name}</h3>
-      <div className="w-48 h-48 border border-black">{nft.image}</div>
+      <div className="w-56 h-56 border border-black mx-auto">{nft.image}</div>
       <div>
         <p>Token Id: {nft.tokenId}</p>
         <p>Contract Address: {nft.contractAddress}</p>
@@ -28,10 +31,27 @@ const nftDisplay = (nft) => {
 }
 
 export const Showcase = () => {
+  const [search, setSearch] = useState("");
+
   const nftList = nfts.map((nft) => { return nftDisplay(nft); });
 
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  }
+
   return (
-    <main className="pt-40 px-10 h-full mx-auto max-w-[1000px]">
+    <main className="pt-40 px-10 h-full mx-auto max-w-[1000px] flex flex-col gap-20">
+      <div
+        className="w-full border border-slate-500 rounded-full flex flex-row gap-5 p-5"
+      >
+        <img src={searchIcon} width={15} height={15}></img>
+        <input 
+        className="w-full outline-none text-lg"
+        onChange={handleSearchChange}
+        type="text" value={search} placeholder="search address ..."
+        >
+        </input>
+      </div>
       <div className="flex flex-row flex-wrap gap-10">
         {nftList}
       </div>
