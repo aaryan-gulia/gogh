9cbea31eecd22df636bff8a5f8465de6ae6e812e
Noella Spitz <noella.spitz@gmail.com>
2024-03-16 11:16:53 +0000

    Add connect wallet button to header

---
+++ frontend/src/components/ConnectButton.jsx
@@ -0,0 +1,28 @@
+import { useWeb3Modal } from '@web3modal/wagmi/react'
+import { useAccount } from 'wagmi'
+
+export default function ConnectButton() {
+  // 4. Use modal hook
+  const { open } = useWeb3Modal()
+  const { address, isConnecting, isDisconnected } = useAccount()
+
+  console.log(address, isConnecting, isDisconnected);
+
+  let btnText = "";
+
+  if (isConnecting) btnText = "Connecting...";
+  else if (isDisconnected) btnText = "Connect Wallet";
+  else btnText = address.substring(0, 8) + '...' + address.substring(address.length - 3);
+
+
+  return (
+    <>
+      <button className='bg-sky-300 rounded-full text-white px-5 h-12 ' 
+      onClick={() => open()}>
+        {btnText}
+      </button>
+    </>
+  )
+}
+
+//<button onClick={() => open({ view: 'Networks' })}>Open Network Modal</button>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,13 @@
+import ConnectButton from "./ConnectButton";
+
 export const Header = () => {
   return (
-    <header className="fixed bg-white w-full">
-      <div className="flex flex-row justify-around">
-        <img src="/goghlogo.png" width={50}></img>
+    <header className="fixed bg-white w-full p-4">
+      <div className="flex flex-row h-20 relative justify-end">
+        <div className="absolute left-1/2">
+          <img className="mx-auto" src="/goghlogo.png" width={50}></img>
+        </div>
+        <ConnectButton />
       </div>
     </header>
   );
