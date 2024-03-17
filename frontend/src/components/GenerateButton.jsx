bfcffe0981b8365a708b8071da9131fa684ed0e6
data4rtist <data4rtist@gmail.com>
2024-03-17 00:17:17 +0000

    Add bitmap for shapes

--- frontend/src/components/GenerateButton.jsx
+++ frontend/src/components/GenerateButton.jsx
@@ -2,31 +2,41 @@ import { useWriteContract } from 'wagmi';
 import { writeContract } from 'viem/actions';
 import abi from "../../abis/IGoghArt.json";
 
-const rectangle = '0b001';
-const triangles = '0b010';
-const circles = '0b100';
+// Define shapes with binary representation as BigInt
+const rectangle = BigInt(0b001);
+const triangles = BigInt(0b010);
+const circles = BigInt(0b100);
+
 
 const shapeBitmaps = {
   "square": rectangle,
   "triangle": triangles,
   "circle": circles,
+};
+
+function shapesToU256(shapes) {
+  let binaryRepresentation = BigInt(0); // Initialize a BigInt with 0
+
+  shapes.forEach(shape => {
+    if (shapeBitmaps.hasOwnProperty(shape)) {
+      binaryRepresentation |= shapeBitmaps[shape]; // Set the corresponding bits
+    }
+  });
+
+  return binaryRepresentation;
 }
 
-export const GenerateButton = ({algorithm, shapes, colours}) => {
-  const { data: hash, writeContract } = useWriteContract()
 
-  const shapeBits = () => {
-    shapes.forEach(shape => {
-      
-    });
-  }
+export const GenerateButton = ({algorithm, numShapes, shapes, colours}) => {
+  const { data: hash, writeContract } = useWriteContract()
+  const shapeBits = shapesToU256(shapes);
 
   const handleGenerate = async () => {
     writeContract({
       address: '0x0EdE555D3f8c3197741A757F9696C7059122D1fc',
       abi,
       functionName: 'getShapes',
-      args: [BigInt()]
+      args: [shapeBits, numShapes]
     });
   }
 
